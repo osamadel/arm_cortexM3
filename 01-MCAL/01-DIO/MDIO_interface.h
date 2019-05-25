c815ce24702236aaf929a4610cb3bf34909bc9b4
Osama Adel <seem.1993@gmail.com>
2019-05-25 12:21:19 +0200

    added configuration to DIO initalize

--- 01-MCAL/01-DIO/MDIO_interface.h
+++ 01-MCAL/01-DIO/MDIO_interface.h
@@ -38,8 +38,35 @@
 #define DIO_U8_PIN33        46
 #define DIO_U8_PIN34        47
 
+
+
 #define DIO_U8_HIGH         1
 #define DIO_U8_LOW          0
+
+
+
+#define DIO_U8_INPUT_ANALOG			0b0000
+#define DIO_U8_INPUT_FLOATING		0b0100
+#define DIO_U8_INPUT_PULLDOWN		0b1000
+#define DIO_U8_INPUT_PULLUP			0b1100
+
+#define DIO_U8_2M_OUT_PP			0b0010
+#define DIO_U8_2M_OUT_OD			0b0110
+#define DIO_U8_2M_OUT_AFPP			0b1010
+#define DIO_U8_2M_OUT_AFOD			0b1110
+
+#define DIO_U8_10M_OUT_PP			0b0001
+#define DIO_U8_10M_OUT_OD			0b0101
+#define DIO_U8_10M_OUT_AFPP			0b1001
+#define DIO_U8_10M_OUT_AFOD			0b1101
+
+#define DIO_U8_50M_OUT_PP			0b0011
+#define DIO_U8_50M_OUT_OD			0b0111
+#define DIO_U8_50M_OUT_AFPP			0b1011
+#define DIO_U8_50M_OUT_AFOD			0b1111
+
+
+
 
 void MDIO_voidInitialize	(void);
 void MDIO_voidSetPinValue 	(u8 Copy_u8PinNo, u8 Copy_u8PinVal);
